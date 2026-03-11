eb722b0da00685d2d03431b45c789b4eb53e2dae
lourdu11 <xavierbrito16@gmail.com>
2026-03-11 23:58:09 +0530

    first commit

--- config/db.js
+++ config/db.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://SproutsOrgs:SproutsOrgs12345@cluster0.1i9dtge.mongodb.net/portfolio?retryWrites=true&w=majority&appName=Cluster0'
-        );
+        const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://SproutsOrgs:SproutsOrgs12345@cluster0.1i9dtge.mongodb.net/portfolio?retryWrites=true&w=majority&appName=Cluster0');
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
